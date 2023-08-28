69b7c9f1eee3dcaff5f4a91ec4fa146a76c4606a
Beachbay <113332198+Beachbay@users.noreply.github.com>
2023-08-28 12:55:06 +0200

    Update Home.jsx

--- src/components/routes/Home.jsx
+++ src/components/routes/Home.jsx
@@ -3,17 +3,43 @@ import BurgerCard from "../BurgerCard";
 
 function Home() {
   const [filterdBurgers, setFilterdBurgers] = useState([]);
+  const [menu, setMenu] = useState([]);
 
   useEffect(() => {
     fetch("http://localhost:7000/menu")
       .then((res) => res.json())
       .then((data) => {
+        setMenu(data);
         const filterdBurgers = data.filter(
           (item) => item.id >= 1 && item.id <= 3
         );
         setFilterdBurgers(filterdBurgers);
       });
   }, []);
+
+  function addToLocalStorage(id) {
+    // letar efter produkten med id:t som skickas in i funktionen
+    const itemToAdd = menu.find((item) => item.id === id);
+    let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+    const itemInCart = cart.find((item) => item.name === itemToAdd.name);
+
+    if (!itemInCart) {
+      const item = {
+        name: itemToAdd.name,
+        price: itemToAdd.price,
+        quantity: 1,
+        image: itemToAdd.image,
+      };
+
+      cart.push(item);
+    } else {
+      itemInCart.quantity++;
+      itemInCart.price += itemToAdd.price;
+    }
+
+    localStorage.setItem("cart", JSON.stringify(cart));
+  }
 
   return (
     <div>
@@ -31,6 +57,7 @@ function Home() {
             id={b.id}
             price={b.price}
             image={b.image}
+            addToLocalStorage={addToLocalStorage}
           />
         ))}
       </div>
